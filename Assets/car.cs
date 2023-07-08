71d899d10b943c266510223fe240529e5c6f8ce4
yusufshalaby <yusufshalaby@gmail.com>
2023-07-08 16:53:48 -0400

    reset button

--- Assets/car.cs
+++ Assets/car.cs
@@ -11,22 +11,26 @@ public class car : MonoBehaviour
     public Vector2 initialDirection;
 
     public float multiplier;
+    public LogicScript logic;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody.velocity = initialDirection * initialSpeed;
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         myRigidBody.velocity = myRigidBody.velocity * multiplier;
         myRigidBody.angularVelocity *= multiplier;
+        int scoreIncrement =  (int)(1000*(myRigidBody.velocity.x + myRigidBody.velocity.y));
+        logic.addScore(scoreIncrement);
         // Vector2 direction = new Vector2(
         //     -1 * Mathf.Sign(initialDirection.x) * Random.value,
         //     Random.value
